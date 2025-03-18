9924db5f21b72a6723149fde77e58e681270c73c
Giovanni Olan Perez <giovanniolanperez.fp@gmail.com>
2025-03-18 20:59:43 +0000

    Refactor sidebar navigation: update nav items to use Font Awesome icons and Angular router links; remove unused items

--- src/app/layouts/dashboard-layout/partials/sidebar-nav/sidebar-nav.component.ts
+++ src/app/layouts/dashboard-layout/partials/sidebar-nav/sidebar-nav.component.ts
@@ -1,62 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'dashboard-layout-sidebar-nav',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar-nav.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarNavComponent {
 
-  public navItems = [
+  public navItems = signal([
     {
-      title: 'Dashboard',
-      description: 'Data Overview',
-      icon: 'dashboard',
-      href: '#',
-      active: true
+      title: 'Trending',
+      description: 'Gifs Populares',
+      icon: 'fa-solid fa-chart-line',
+      href: '/dashboard/trending',
     },
     {
-      title: 'Database',
-      description: 'Database Manager',
-      icon: 'database',
-      href: '#',
-      active: false
+      title: 'Buscador',
+      description: 'Buscar gifs',
+      icon: 'fa-solid fa-magnifying-glass',
+      href: '/dashboard/search',
     },
-    {
-      title: 'Cloud Storage',
-      description: 'Manage Cloud Storage',
-      icon: 'cloud',
-      href: '#',
-      active: false
-    },
-    {
-      title: 'Reports',
-      description: 'Manage Reports',
-      icon: 'report',
-      href: '#',
-      active: false
-    },
-    {
-      title: 'Security',
-      description: 'Generate Security Keys',
-      icon: 'security',
-      href: '#',
-      active: false
-    },
-    {
-      title: 'Extensions',
-      description: 'Manage Extensions',
-      icon: 'extensions',
-      href: '#',
-      active: false
-    },
-    {
-      title: 'Settings',
-      description: 'Edit App Settings',
-      icon: 'settings',
-      href: '#',
-      active: false
-    }
-  ];
+  ]);
 }
